8b37d7ed996ef498d93be7bdd864b60ef37f0012
mariusmoldovan <biris_21@yahoo.com>
2021-07-21 21:24:07 -0500

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -126,6 +126,8 @@ class Car {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
     // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -143,6 +145,15 @@ class Car {
    */
   drive(distance) {
     // ✨ implement
+   const milesToDrive = this.tank * this.mpg
+   if (distance <= milesToDrive) {
+     this.odometer =this.odometer + distance
+     this.tank = this.tank - (distance / this.mpg)
+   } else {
+     this.tank = 0
+     this.odometer = this.odometer + milesToDrive
+   }
+   return this.odometer
   }
 
   /**
@@ -158,6 +169,13 @@ class Car {
    */
   refuel(gallons) {
     // ✨ implement
+
+    if(gallons <= this.tankSize - this.tank) {
+      this.tank = this.tank + gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
@@ -174,8 +192,9 @@ class Car {
  *    // result is false
  * })
  */
-function isEvenNumberAsync(number) {
+async function isEvenNumberAsync(number) {
   // ✨ implement
+  return number % 2 === 0 || false
 }
 
 module.exports = {
